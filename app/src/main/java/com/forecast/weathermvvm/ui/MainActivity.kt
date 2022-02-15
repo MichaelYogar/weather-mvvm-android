6ecd1291b5dc681df109b9125f5e06015572d6be
Michael Yogar <michaelyogar878@gmail.com>
2022-02-14 20:10:52 -0500

    foundation for navigation

---
+++ app/src/main/java/com/forecast/weathermvvm/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.forecast.weathermvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.forecast.weathermvvm.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.bottom_nav)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+        bottomNav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
